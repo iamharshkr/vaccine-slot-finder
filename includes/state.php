68bff07a2b6f75cd92b09f1c8dc671f9ea901496
iamharshkr <54257619+iamharshkr@users.noreply.github.com>
2021-05-14 10:57:05 +0530

    Update state.php

--- includes/state.php
+++ includes/state.php
@@ -3,16 +3,8 @@ if(!empty($_POST["dist"])){
     $dist= $_POST['dist'];
     $date = $_POST['date']; //date format - dd-mm-yyy
     $idpin = date('Y-m-d', strtotime($date));
-$curl = curl_init();
-    curl_setopt_array($curl, array(
-        CURLOPT_URL => 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=' .$dist. '&date=' .$date,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_TIMEOUT => 30,
-        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-        CURLOPT_CUSTOMREQUEST => "GET",
-    ));
-    $response = curl_exec($curl);
-    $resultToJson = json_decode($response, true);
+        $url = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=' .$dist. '&date=' .$date;
+    $resultToJson = get_data($url);
     if (!isset($resultToJson['sessions'])) {
       ?>
         <h3>Something went error, Please try again.</h3>
@@ -21,8 +13,11 @@ $curl = curl_init();
     }
     if(count($resultToJson['sessions']) > 0){
       ?>
-            <h2 class="slot">Slot Details for <?php echo $date ?></h2>
-            <div class="scroll">       
+<h2 class="slot">Slot Details for <?php echo $date ?></h2>
+<button id="show-all" class="filter-button clicked">Show All</button>
+<button id="18plus" class="filter-button">18+</button>
+<button id="45plus" class="filter-button">45+</button> 
+<div class="scroll">       
   <table class="table table-bordered">
     <thead>
       <tr>
@@ -47,12 +42,30 @@ $curl = curl_init();
         $Minimum_Age = $session['min_age_limit'];
         $Slots = $session['available_capacity'];
         if(isset($session['vaccine'])){
+        $Timing = '<ul>';
         $Vaccine = $session['vaccine'];
-        $Timing = '<li>' .$session['slots'][0] ."</li><li>". $session['slots'][1] ."</li><li>". $session['slots'][2] ."</li>";
+        if(isset($session['slots'][0])){
+        $Timing .= '<li>' .$session['slots'][0] .'</li>';
+        }
+        if(isset($session['slots'][1])){
+        $Timing .= '<li>'. $session['slots'][1] .'</li>';
+        }
+        if(isset($session['slots'][2])){
+        $Timing .= '<li>'. $session['slots'][2] .'</li></ul>';
+        }
         }
         ?>
-        <tr>
-        <td><?php echo $cname ?></td>
+        <tr class="<?php echo $Minimum_Age ?>">
+        <td><?php echo $cname ?>
+        <?php
+        if($Slots > 0){
+          ?>
+        <br>
+        <a href="https://selfregistration.cowin.gov.in/" target="_blank"><button class="btn btn-primary">Book Your Slot</button></a>
+        <?php
+        }
+        ?>
+        </td>
         <td><?php echo $Minimum_Age ?></td>
         <?php
         if($Slots > 0){
@@ -61,7 +74,7 @@ $curl = curl_init();
         <?php
         }else{
           ?>
-          <td class="danger"><?php echo $Slots ?></td>
+          <td class="danger">N/A</td>
           <?php
         }
         ?>
@@ -84,5 +97,20 @@ $curl = curl_init();
     <?php
   }
   }
-      ?>
+  function get_data($url) {
+    $ch = curl_init();
+   curl_setopt($ch, CURLOPT_URL, $url);
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+   //curl_setopt($ch, CURLOPT_PROXY, $proxy); // $proxy is ip of proxy server
+   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+   curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+   curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+   curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+   $httpCode = curl_getinfo($ch , CURLINFO_HTTP_CODE); // this results 0 every time
+   $response = curl_exec($ch);
+   if ($response === false) $response = curl_error($ch);
+   curl_close($ch);
+  return json_decode($response, true);
+  }
+?>
       
